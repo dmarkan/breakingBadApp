67e493b8d90f382f4fd6f8bab4eefd3627b320c0
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-11-23 18:13:31 +0100

    Second commit

--- index.js
+++ index.js
@@ -26,19 +26,21 @@ app.post("/", function (req, res) {
             const charStatus = breakingBadData[0].status;
             const charPortrayed = breakingBadData[0].portrayed;
             const charAppearance = breakingBadData[0].appearance;
-            res.write("<h2>" + charName + "</h2>");
-            res.write("<img src=" + charImg + ">");
-            res.write("<p>Nickname: " + charNick + "</p>");
-            res.write("<p>Date of birth: " + charBirthday + "</p>");
-            res.write("<p>Occupation: " + charOccupation + "</p>");
-            res.write("<p>Status: " + charStatus + "</p>");
-            res.write("<p>Portrayed by: " + charPortrayed + "</p>");
-            res.write("<p>Appeared in seasons: " + charAppearance + "</p>");
+            res.setHeader("Content-Type", "text/html");   // res.write not working properly. It's showing output including HTML tags --- to FIX this problem
+            res.write("<body style='background-image: url(images/background.png); background-size: cover; background-attachment: fixed; background-repeat: no-repeat;'>")
+            res.write("<h1 style='text-align:center;color:#fff;font-size:5rem;'>" + charName + "</h1>");
+            res.write("<img style='width:30rem;height:35rem;margin: auto;display: block;' src=" + charImg + "><br>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Nickname: " + charNick + "</p>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Date of birth: " + charBirthday + "</p>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Occupation: " + charOccupation + "</p>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Status: " + charStatus + "</p>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Portrayed by: " + charPortrayed + "</p>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:1.5rem;'>Appeared in seasons: " + charAppearance + "</p>");
             res.send();
         })
-    })
+    });
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 })
